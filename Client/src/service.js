4ff5f3588b2693e5b2fbf3c89f928085886445ae
Brurya-Zarbiv <bz0556758244@gmail.com>
2025-02-11 13:51:05 +0200

    Update Client

--- Client/src/service.js
+++ Client/src/service.js
@@ -10,30 +10,30 @@ export default {
   },
 
   getTasks: async () => {
-    const result = await axios.get(`${apiUrl}/items`)
+    const result = await axios.get(`${apiUrl}/tasks`)
     return result.data;
   },
 
   getTaskById: async (id) => {
-    const result = await axios.get(`${apiUrl}/items/${id}`)
+    const result = await axios.get(`${apiUrl}/tasks/${id}`)
     return result.data;
   },
 
   addTask: async (name) => {
     console.log('addTask', name)
-    const result = await axios.post(`${apiUrl}/items`, { name })
+    const result = await axios.post(`${apiUrl}/tasks`, { name })
     return result.data;
   },
 
   setCompleted: async (id, isComplete) => {
     console.log('setCompleted', { id, isComplete })
-    const result = await axios.put(`${apiUrl}/items/${id}`, { isComplete })
+    const result = await axios.put(`${apiUrl}/tasks/${id}`, { isComplete })
     return result.data;
   },
 
   deleteTask: async (id) => {
     console.log('deleteTask', id)
-    const result = await axios.delete(`${apiUrl}/items/${id}`)
+    const result = await axios.delete(`${apiUrl}/tasks/${id}`)
     return result.data;
   }
 }
